65faeabf20d45e0c2de859b6ba4374ecf462bc09
Aahil13 <onyeanunaprince@example.com>
2023-11-05 16:22:00 +0100

    Add visitor count functionality to resume page

---
+++ src/script.js
@@ -0,0 +1,18 @@
+fetch(
+  "https://g0u5nj2zi1.execute-api.us-east-1.amazonaws.com/dev/resume_counter",
+  {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }
+)
+  .then((response) => response.json())
+  .then((data) => {
+    var count = data.N;
+    let editedText = count == 1 ? "1 visitor" : count + " visitors";
+    document.getElementById("visitor-count-element").textContent = editedText;
+  })
+  .catch((error) => {
+    console.error("Error fetching visitor count:", error);
+  });
