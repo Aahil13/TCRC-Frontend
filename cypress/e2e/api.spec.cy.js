79fe09c59830d6748c15706caeced5f641ced80c
Aahil13 <onyeanunaprince@example.com>
2023-11-05 16:22:10 +0100

    Update API endpoint URL.

--- cypress/e2e/api.spec.cy.js
+++ cypress/e2e/api.spec.cy.js
@@ -2,7 +2,7 @@ describe("TEST API", () => {
   it("Should return an updated value and update the database", () => {
     // Make a request to your API endpoint
     cy.request(
-      "https://n2j0ubj5wl.execute-api.us-east-1.amazonaws.com/default/resume_counter"
+      "https://g0u5nj2zi1.execute-api.us-east-1.amazonaws.com/dev/resume_counter"
     ).then((response) => {
       // Verify the response status code
       expect(response.status).to.equal(200);
