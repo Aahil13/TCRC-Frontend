327a89d1d5a78c10826fa867ce975100f9efc6e3
Aahil13 <onyeanunaprince@example.com>
2023-11-06 14:10:44 +0100

    Update cypress.config.js specPattern.

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
     supportFile:
       "/home/runner/work/TCRC-Frontend/TCRC-Frontend/cypress/support/e2e/api.spec.cy.js",
-    specPattern: "e2e/*.cy.{js,jsx,ts,tsx}",
+    specPattern: "cypress/support/e2e/api.spec.cy.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
