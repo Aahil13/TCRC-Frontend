8f9bf4d0fbafe93210cdccaa1a0cdb477d0a8a21
Aahil13 <onyeanunaprince@example.com>
2023-11-06 14:24:32 +0100

    Update specPattern path in cypress.config.js.

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     supportFile: "cypress/support",
-    specPattern: "e2e/api.spec.cy.js",
+    specPattern: "cypress/support/e2e/api.spec.cy.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
