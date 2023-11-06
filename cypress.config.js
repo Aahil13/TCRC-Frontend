01bd216d1c97a40ab2f6c8ce927952add5566f2a
Aahil13 <onyeanunaprince@example.com>
2023-11-06 14:08:15 +0100

    Update support file path in cypress.config.js.

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     supportFile:
-      "/home/runner/work/TCRC-Frontend/TCRC-Frontend/cypress/support/api.spec.cy.js",
+      "/home/runner/work/TCRC-Frontend/TCRC-Frontend/cypress/support/e2e/api.spec.cy.js",
     specPattern: "e2e/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
