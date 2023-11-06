fb21f3350b9ab2199f3be2080f8924a86b58c008
Aahil13 <onyeanunaprince@example.com>
2023-11-06 14:05:18 +0100

    Fix environment name in smoke_test job and update cypress support file path.

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: "cypress/support/api.spec.cy.js",
+    supportFile:
+      "/home/runner/work/TCRC-Frontend/TCRC-Frontend/cypress/support/api.spec.cy.js",
     specPattern: "e2e/*.cy.{js,jsx,ts,tsx}",
     setupNodeEvents(on, config) {
       // implement node event listeners here
