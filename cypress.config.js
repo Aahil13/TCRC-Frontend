e9c008f619e1c833ac6f60ce76461d431d899b32
Aahil13 <onyeanunaprince@example.com>
2023-11-06 14:20:33 +0100

    Update Cypress config for e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    supportFile:
-      "/home/runner/work/TCRC-Frontend/TCRC-Frontend/cypress/support/e2e/api.spec.cy.js",
-    specPattern: "cypress/support/e2e/api.spec.cy.js",
+    supportFile: "cypress/support",
+    specPattern: "e2e/api.spec.cy.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
